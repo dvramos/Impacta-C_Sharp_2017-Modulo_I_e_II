7974c825638b2cec988b1ec9bb9fd7aa794b0b20
dvramos <dvramos@hotmail.com.br>
2022-04-09 20:49:13 -0300

    Aula03-Atividade01

---
+++ Aula03-ProjetoConsole/Atividade01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Daniel Ramos");
+            Console.WriteLine("daniel@email.com.br");
+            Console.WriteLine("(11) 12345-6789");
+
+            Console.ReadLine();
+        }
+    }
+}
