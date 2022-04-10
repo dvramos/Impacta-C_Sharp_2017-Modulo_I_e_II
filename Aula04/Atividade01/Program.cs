bb1071891880f514ac43a9efc95da151269b975c
dvramos <dvramos@hotmail.com.br>
2022-04-09 21:16:03 -0300

    Aula 04 - Introdução a Variaveis

---
+++ Aula04/Atividade01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade01
+{
+    //Classe
+    internal class Program
+    {
+        //Método Principal
+        static void Main(string[] args)
+        {
+            //Somar dois números
+            int n1, n2, resultado;
+
+            //Obtendo os valores
+            Console.Write("Digite o primeiro número: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            //Processando
+            resultado = n1 + n2;
+
+            //Exibindo
+            Console.WriteLine("A soma de {0} e {1} é {2}!",n1, n2, resultado);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Aula04/ObtendoInformacoesDoUsuario/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObtendoInformacoesDoUsuario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+
+            Console.WriteLine("Digite o seu nome!");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o sua idade!");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            int dias = idade * 365;
+
+            Console.WriteLine("Olá {0} você já viveu {1} dias.", nome, dias);
+            Console.ReadLine();
+        }
+    }
+}
