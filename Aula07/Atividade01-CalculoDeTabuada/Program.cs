38044dd2e52e4e5398ec6ec7f04766f3476c9397
dvramos <dvramos@hotmail.com.br>
2022-04-13 12:16:33 -0300

    Aula07 - Estruturas de Repetição

---
+++ Aula07/Atividade01-CalculoDeTabuada/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade01_CalculoDeTabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabuada;
+            Console.WriteLine("Qual é a tabuada desejada? ");
+            tabuada = Convert.ToInt32(Console.ReadLine());
+            string valor = string.Empty;
+
+            do
+            {
+                if (valor != string.Empty)
+                {
+                    tabuada = Convert.ToInt32(valor);
+                }
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine("{0} x {1} = {2}", tabuada, i, i * tabuada);
+                }
+                Console.WriteLine();
+                Console.Write("Digite a tabuada ou enter para sair.");
+
+                valor = Console.ReadLine();
+            } while (valor != string.Empty);
+                Console.ReadLine();
+
+        }
+    }
+}
---
+++ Aula07/EstruturaDeRepeticao/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturaDeRepeticao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (inicio; condição; incremento) {}
+
+            for (int i = 0; i<=10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            
+            //while (condição)
+
+            string senha = "123";
+            string senhaInformada = string.Empty;
+
+            while (senha != senhaInformada)
+            {
+                Console.WriteLine("Digite a senha: ");
+                senhaInformada = Console.ReadLine();
+            }
+            Console.WriteLine("Senha Correta!");
+
+            int x = 1;
+            while (x <= 10)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+
+            // do while
+
+            string senha1 = "123";
+            string senhaInformada1 = string.Empty;
+
+            do
+            {
+                Console.WriteLine("Digite a senha: ");
+                senhaInformada1 = Console.ReadLine();
+                if (senhaInformada1 != senha1)
+                {
+                    Console.WriteLine("Senha Incorreta!");
+                }
+            }
+            while (senha1 != senhaInformada1);
+            {
+                
+            }
+            Console.WriteLine("Senha Correta!");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
