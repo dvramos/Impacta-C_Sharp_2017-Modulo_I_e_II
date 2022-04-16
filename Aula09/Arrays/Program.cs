c37e4b25dac07b6fe0299c44f067113638861c0f
dvramos <dvramos@hotmail.com.br>
2022-04-15 23:54:41 -0300

    Aula 09 - Arrays e Coleções

---
+++ Aula09/Arrays/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array
+
+            int[] numeros = new int[6];
+
+            //Preenchendo
+            numeros[0] = 40;
+            numeros[1] = 5;
+            numeros[2] = 52;
+            numeros[3] = 17;
+            numeros[4] = 6;
+            numeros[5] = 10;
+
+            //Percorrendo
+            foreach(int n  in numeros)
+            {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine("---------");
+
+            //Ordenar
+            Array.Sort(numeros);
+
+            //Percorrendo
+            foreach (int n in numeros)
+            {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine("---------");
+
+            //Pesquisar
+            int indice = Array.IndexOf(numeros, 40);
+            if (indice == -1)
+            {
+                Console.WriteLine("40 não encontrado!");
+            }
+            else
+            {
+                Console.WriteLine("Encontrado:{0} na posição {1}",numeros[indice], indice);
+            }
+            Console.WriteLine("---------");
+
+            //Inverter
+            Array.Reverse(numeros);
+
+            foreach (int n in numeros)
+            {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine("---------");
+
+            //Tamanho
+            int tamanho = numeros.Length;
+            Console.WriteLine("Tamanho:{0}", tamanho);
+
+            Console.WriteLine("---------");
+
+            string[] arquivos = Directory.GetFiles(@"C:\windows\web", ".", SearchOption.AllDirectories);
+            foreach (string arq in arquivos)
+            {
+                Console.WriteLine(arq);
+            }
+           
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aula09/Colecoes/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Colecoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList lista = new ArrayList();
+            lista.Add("São Paulo");
+            lista.Add("Rio de Janeiro");
+            lista.Add("Santos");
+         // lista.Add(10);  Permite inserir e não dá erro de compilação, apenas na execução
+
+            string cidade = Convert.ToString(lista[1]);
+
+            foreach (string cid in lista)
+            {
+                Console.WriteLine(cid);
+            }
+            Console.WriteLine("---------");
+
+            lista.Remove("Santos");
+            lista.Insert(0,"Londres");
+            foreach (string cid in lista)
+            {
+                Console.WriteLine(cid);
+            }
+            Console.WriteLine("---------");
+
+            int tamanho = lista.Count;
+            Console.WriteLine("Tamanho: {0}", tamanho);
+            Console.WriteLine("---------");
+
+
+            List<string> cidades = new List<string>();
+            cidades.Add("São Paulo");
+            cidades.Add("Rio de Janeiro");
+            cidades.Add("Santos");
+
+
+            List<int> numeros = new List<int>();
+            numeros.Add(10);
+            numeros.Add(20);
+            numeros.Add(16);
+
+            foreach ( string c in cidades )
+            {
+                Console.WriteLine(c);
+            }
+            Console.WriteLine("---------");
+
+            foreach (int n in numeros)
+            {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine("---------");
+
+            // Fila - FIFO -> First In First Out
+            Queue   <string> fila = new Queue<string>();
+            fila.Enqueue("Processo 1");
+            fila.Enqueue("Processo 2");
+            fila.Enqueue("Processo 3");
+
+            while (fila.Count > 0)
+            {
+                string item = fila.Dequeue();
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------");
+
+            // Pilha - FILO -> First In Last Out
+            Stack<string> pilha = new Stack<string>();
+            pilha.Push("Processo 1");
+            pilha.Push("Processo 2");
+            pilha.Push("Processo 3");
+
+            while (pilha.Count > 0)
+            {
+                string item2 = pilha.Pop();
+                Console.WriteLine(item2);
+            }
+            Console.WriteLine("---------");
+
+
+            // Dictionary - 
+
+            Dictionary<string, string> dados = new Dictionary<string, string>();
+
+            dados.Add("Nome", "José da Silva");
+            dados.Add("RG", "23.234.1234");
+            dados.Add("Telefone", "(11) 91234-4321");
+
+            Console.WriteLine(dados["Nome"]);
+            Console.WriteLine(dados["RG"]);
+            Console.WriteLine(dados["Telefone"]);
+
+            Console.WriteLine("---------");
+
+
+            Console.ReadLine();
+        }
+    }
+}
