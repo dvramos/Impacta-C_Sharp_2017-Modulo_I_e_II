7f7fac2e3193b5429f0c9ce63aff42532957e615
dvramos <dvramos@hotmail.com.br>
2022-04-16 23:27:51 -0300

    Aula10-POO
    Conceitos - Metódos - Campos e Propriedades.

---
+++ Aula10/POO-Conceitos/Calculos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Conceitos
+{
+    public class Calculos
+    {
+        // Método
+        public int Somar(int a, int b, int c)
+        {
+            int resultado = a + b + c;
+            return resultado;
+        }
+
+        // Método
+        public int Subtrair(    int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ Aula10/POO-Conceitos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Conceitos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criando um objeto/instância da classe Calculos
+
+            Calculos calc = new Calculos();
+            int total = calc.Somar(1, 5, 15);
+            int resto = calc.Subtrair(10, 3);
+
+            Console.WriteLine("Total: {0}", total);
+            Console.WriteLine("Resto: {0}", resto);
+
+
+            Produto prod = new Produto();
+            prod.Nome = "Mo";
+            prod.Preco = -100;
+
+            Console.WriteLine("Nome: {0}", prod.Nome);
+            Console.WriteLine("Preço: {0:c}", prod.Preco);
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
